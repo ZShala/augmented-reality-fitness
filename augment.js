a6786f148ba0ef936b921b1595020ba092522e0e
ZShala <zanashala2015@gmail.com>
2020-01-13 10:23:09 +0100

    Squat Exercise Finished

---
+++ augment.js
@@ -0,0 +1,74 @@
+let video;
+let poseNet;
+let pose;
+let skeleton;
+
+function setup() {
+    createCanvas(640, 480);
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(poses) {
+    //console.log(poses);
+    if (poses.length > 0) {
+        pose = poses[0].pose;
+        skeleton = poses[0].skeleton;
+    }
+}
+
+function modelLoaded() {
+    console.log('poseNet ready');
+}
+
+function draw() {
+    image(video, 0, 0);
+
+    if (pose) {
+        //let eyeR = pose.rightEye;
+        //let eyeL = pose.leftEye;
+        //let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
+        var dx1 = pose.rightHip.x - pose.rightKnee.x;
+        var dy1 = pose.rightHip.y - pose.rightKnee.y;
+        var dx2 = pose.rightKnee.x - pose.rightAnkle.x;
+        var dy2 = pose.rightKnee.y - pose.rightAnkle.y;
+        var a1 = Math.atan2(dy1, dx1);
+        var a2 = Math.atan2(dy2, dx2);
+        var a = parseInt((a2 - a1) * 180 / Math.PI + 360) % 360;
+        console.log(a)
+        if (a >= 60 && a <= 90) {
+            for (let i = 0; i < pose.keypoints.length; i++) {
+                let x = pose.keypoints[i].position.x;
+                let y = pose.keypoints[i].position.y;
+                fill(71, 211, 202);
+                ellipse(x, y, 16, 16);
+            }
+
+            for (let i = 0; i < skeleton.length; i++) {
+                let a = skeleton[i][0];
+                let b = skeleton[i][1];
+                strokeWeight(1);
+                stroke(71, 211, 202);
+                line(a.position.x, a.position.y, b.position.x, b.position.y);
+            }
+        } else {
+
+            for (let i = 0; i < pose.keypoints.length; i++) {
+                let x = pose.keypoints[i].position.x;
+                let y = pose.keypoints[i].position.y;
+                fill(255, 0, 0);
+                ellipse(x, y, 16, 16);
+            }
+
+            for (let i = 0; i < skeleton.length; i++) {
+                let a = skeleton[i][0];
+                let b = skeleton[i][1];
+                strokeWeight(2);
+                stroke(255, 0, 0);
+                line(a.position.x, a.position.y, b.position.x, b.position.y);
+            }
+        }
+    }
+}
