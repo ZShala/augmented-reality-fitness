8b085d6d7df96737b1122622a53618c4f525b85b
ZShala <zanashala2015@gmail.com>
2020-01-19 23:24:53 +0100

    Arrow Functions

--- augment.js
+++ augment.js
@@ -7,29 +7,22 @@ function setup() {
     createCanvas(640, 480);
     video = createCapture(VIDEO);
     video.hide();
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
-}
-
-function gotPoses(poses) {
-    // console.log(poses);
-    if (poses.length > 0) {
-        pose = poses[0].pose;
-        skeleton = poses[0].skeleton;
-    }
-}
-
-function modelLoaded() {
-    console.log('poseNet ready');
+    poseNet = ml5.poseNet(video, () => {
+        console.log('poseNet ready');
+    });
+    poseNet.on('pose', (poses) => {
+        // console.log(poses);
+        if (poses.length > 0) {
+            pose = poses[0].pose;
+            skeleton = poses[0].skeleton;
+        }
+    });
 }
 
 function draw() {
     image(video, 0, 0);
 
     if (pose) {
-        //let eyeR = pose.rightEye;
-        //let eyeL = pose.leftEye;
-        //let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
         var dx1 = pose.rightHip.x - pose.rightKnee.x;
         var dy1 = pose.rightHip.y - pose.rightKnee.y;
         var dx2 = pose.rightKnee.x - pose.rightAnkle.x;
@@ -43,7 +36,7 @@ function draw() {
                 let x = pose.keypoints[i].position.x;
                 let y = pose.keypoints[i].position.y;
                 fill(71, 211, 202);
-                ellipse(x, y, 16, 16);
+                ellipse(x, y, 8, 8);
             }
 
             for (let i = 0; i < skeleton.length; i++) {
@@ -59,7 +52,7 @@ function draw() {
                 let x = pose.keypoints[i].position.x;
                 let y = pose.keypoints[i].position.y;
                 fill(255, 0, 0);
-                ellipse(x, y, 16, 16);
+                ellipse(x, y, 8, 8);
             }
 
             for (let i = 0; i < skeleton.length; i++) {
