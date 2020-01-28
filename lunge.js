10b29406fa60ffbe2ba880623aaf2b6d5d154572
ZShala <zanashala2015@gmail.com>
2020-01-28 23:39:43 +0100

    More Exercises

--- lunge.js
+++ lunge.js
@@ -1,13 +1,13 @@
-let camera;
+let video;
 let model;
 let pose;
 let skeleton;
 
 function setup() {
     createCanvas(640, 480);
-    camera = createCapture(VIDEO);
-    camera.hide();
-    model = ml5.poseNet(camera, () => {
+    video = createCapture(VIDEO);
+    video.hide();
+    model = ml5.poseNet(video, () => {
         console.log('PoseNet Model Loaded');
     });
     model.on('pose', (poses) => {
@@ -19,29 +19,37 @@ function setup() {
 }
 
 function draw() {
-    image(camera, 0, 0);
+    image(video, 0, 0);
 
     if (pose) {
-        let dx1 = pose.rightHip.x - pose.rightKnee.x;
-        let dy1 = pose.rightHip.y - pose.rightKnee.y;
-        let dx2 = pose.rightKnee.x - pose.rightAnkle.x;
-        let dy2 = pose.rightKnee.y - pose.rightAnkle.y;
-        let a1 = Math.atan2(dy1, dx1);
-        let a2 = Math.atan2(dy2, dx2);
-        let a = parseInt((a2 - a1) * 180 / Math.PI + 360) % 360;
+        let rx1 = pose.rightHip.x - pose.rightKnee.x;
+        let ry1 = pose.rightHip.y - pose.rightKnee.y;
+        let rx2 = pose.rightKnee.x - pose.rightAnkle.x;
+        let ry2 = pose.rightKnee.y - pose.rightAnkle.y;
+        let lx1 = pose.leftHip.x - pose.leftKnee.x;
+        let ly1 = pose.leftHip.y - pose.leftKnee.y;
+        let lx2 = pose.leftKnee.x - pose.leftAnkle.x;
+        let ly2 = pose.leftKnee.y - pose.leftAnkle.y;
 
-        if (a >= 100 && a <= 185) {
+        let a1 = Math.atan2(ry1, rx1);
+        let a2 = Math.atan2(ry2, rx2);
+        let b1 = Math.atan2(ly1, lx1);
+        let b2 = Math.atan2(ly2, lx2);
+        let a = parseInt((a2 - a1) * 180 / Math.PI + 360) % 360;
+        let b = parseInt((b2 - b1) * 180 / Math.PI + 360) % 360;
+
+        if ((a >= 45 && a <= 90) && (b >= 100 && b <= 185)) {
             for (let i = 0; i < pose.keypoints.length; i++) {
                 let x = pose.keypoints[i].position.x;
                 let y = pose.keypoints[i].position.y;
                 fill(71, 211, 202);
-                ellipse(x, y, 8, 8);
+                ellipse(x, y, 16, 16);
             }
 
             for (let i = 0; i < skeleton.length; i++) {
                 let a = skeleton[i][0];
                 let b = skeleton[i][1];
-                strokeWeight(1);
+                strokeWeight(2);
                 stroke(71, 211, 202);
                 line(a.position.x, a.position.y, b.position.x, b.position.y);
             }
@@ -51,13 +59,13 @@ function draw() {
                 let x = pose.keypoints[i].position.x;
                 let y = pose.keypoints[i].position.y;
                 fill(255, 0, 0);
-                ellipse(x, y, 8, 8);
+                ellipse(x, y, 16, 16);
             }
 
             for (let i = 0; i < skeleton.length; i++) {
                 let a = skeleton[i][0];
                 let b = skeleton[i][1];
-                strokeWeight(1);
+                strokeWeight(2);
                 stroke(255, 0, 0);
                 line(a.position.x, a.position.y, b.position.x, b.position.y);
             }
---
+++ tricep.js
@@ -0,0 +1,66 @@
+let video;
+let model;
+let pose;
+let skeleton;
+
+function setup() {
+    createCanvas(640, 480);
+    video = createCapture(VIDEO);
+    video.hide();
+    model = ml5.poseNet(video, () => {
+        console.log('PoseNet Model Loaded');
+    });
+    model.on('pose', (poses) => {
+        if (poses.length > 0) {
+            pose = poses[0].pose;
+            skeleton = poses[0].skeleton;
+        }
+    });
+}
+
+function draw() {
+    image(video, 0, 0);
+
+    if (pose) {
+        let dx1 = pose.rightShoulder.x - pose.rightWrist.x;
+        let dy1 = pose.rightShoulder.y - pose.rightWrist.y;
+        let dx2 = pose.rightShoulder.x - pose.rightHip.x;
+        let dy2 = pose.rightShoulder.y - pose.rightHip.y;
+        let a1 = Math.atan2(dy1, dx1);
+        let a2 = Math.atan2(dy2, dx2);
+        let a = parseInt((a2 - a1) * 180 / Math.PI + 360) % 360;
+
+        if (a >= 315 && a <= 330) {
+            for (let i = 0; i < pose.keypoints.length; i++) {
+                let x = pose.keypoints[i].position.x;
+                let y = pose.keypoints[i].position.y;
+                fill(71, 211, 202);
+                ellipse(x, y, 16, 16);
+            }
+
+            for (let i = 0; i < skeleton.length; i++) {
+                let a = skeleton[i][0];
+                let b = skeleton[i][1];
+                strokeWeight(2);
+                stroke(71, 211, 202);
+                line(a.position.x, a.position.y, b.position.x, b.position.y);
+            }
+        } else {
+
+            for (let i = 0; i < pose.keypoints.length; i++) {
+                let x = pose.keypoints[i].position.x;
+                let y = pose.keypoints[i].position.y;
+                fill(255, 0, 0);
+                ellipse(x, y, 16, 16);
+            }
+
+            for (let i = 0; i < skeleton.length; i++) {
+                let a = skeleton[i][0];
+                let b = skeleton[i][1];
+                strokeWeight(2);
+                stroke(255, 0, 0);
+                line(a.position.x, a.position.y, b.position.x, b.position.y);
+            }
+        }
+    }
+}
